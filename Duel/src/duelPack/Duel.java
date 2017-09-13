14310e1f4d80e7024d11281291b6f8926c340442
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-09-13 15:29:04 -0400

    hello??

--- Duel/src/duelPack/Duel.java
+++ Duel/src/duelPack/Duel.java
@@ -1,7 +1,27 @@
 package duelPack;
 
+import duel.CharacterA;
+import duel.CharacterB;
+import duel.Duel;
+import duel.Dueler;
+
 public class Duel {
-	//code
-	//more code
-	//even mroe code
+
+
+	private Dueler d1;
+	private Dueler d2;
+	
+	public static void main(String[] args){
+		Duel duel = new Duel();
+		duel.dukeItOut();
+	}
+	
+	public Duel() {
+		d1 = new CharacterA();
+		d2 = new CharacterB();
+	}
+	
+	public void dukeItOut(){
+		
+	}
 }
