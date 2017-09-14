d01d3eeb1cc1495c39b0d6cb6c40798be076e463
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2017-09-14 14:40:41 -0400

    asdasd

--- Duel/src/duelPack/CharacterA.java
+++ Duel/src/duelPack/CharacterA.java
@@ -1,7 +1,16 @@
 package duelPack;
 
-public class CharacterA {
-
+public class CharacterA implements Dueler{
+	
+	public CharacterA() {
+		
+	}
+	
+	void taunt();
+	String getName();
+	void getStartingHP(int hp);
+	int getHP();
+	
 }
 
 
