9a09ac6442050e428a7b256ba147b6aa7da88d3a
Tristan Kyle Chau <Tristan Kyle Chau@192.168.10.105>
2017-09-17 22:02:02 -0400

    Update from home

--- Duel/src/duelPack/CharacterB.java
+++ Duel/src/duelPack/CharacterB.java
@@ -3,18 +3,25 @@ package duelPack;
 public class CharacterB implements Dueler
 {
 	private String name;
-	private int HP;
+	private int currentHP;
+	String[] taunt = {"lol u suck, get better nerd, ur garbage"};
 	
-	public CharacterB(String name, int HP)
+	public CharacterB()
 	{
-		this.name = "Ur mom";
-		this.HP = 100;
+		this.name = "Marth";
 	}
-	public void taunt() 
+	public void taunt()
 	{
-		String[] taunt = {"lol u suck, get better nerd, ur garbage"};
-		System.out.println(taunt);
+		int randTaunt = (int)(Math.random()*taunts.length);
 		
+		if(randTaunt != 1)
+		{	
+			System.out.println("Marth, the better character says, '" + taunts[rand] + "'");
+		}
+		else 
+		{
+			System.out.println(taunts[randTaunt]);
+		}
 	}
 	public String getName()
 	{
@@ -22,15 +29,38 @@ public class CharacterB implements Dueler
 	}
 	public void setStartingHP(int hp)
 	{
-		HP = hp;
+		currentHP = hp;
 	}
-	public int getHP();
-	{ 
-		System.out.println(HP);
-		return HP;
-	}
-	public void getAction()
+	public int getHP()
 	{
+		return currentHP;
+	}
+	public int getAction(Object caller)
+	{	
+		if(caller instanceof Duel)
+		{
+			int rand = (int)(Math.random()*3);
+			return rand;
+		}
 		
+		return 3;
+	}
+	
+	public void hit(Object caller) 
+	{
+		if(caller instanceof Duel)
+		{
+			this.currentHP = currentHP - 10;
+		}
+	}
+	
+	public boolean determineIfOpponentIsFair(Dueler d, int hp)
+	{
+		if(d.getHP() != hp)
+		{
+			return false;
+		}
+		
+		return true;
 	}
 }
