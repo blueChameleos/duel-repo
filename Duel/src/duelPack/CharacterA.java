30d918d587bffbb0eae30e10542514397f85c19d
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-09-25 14:45:00 -0400

    edited char A

--- Duel/src/duelPack/CharacterA.java
+++ Duel/src/duelPack/CharacterA.java
@@ -4,6 +4,8 @@ public class CharacterA implements Dueler{
 	
 	private String Name;
 	private int currentHP;
+	private boolean isLoaded = false;
+	
 	String[] taunts = {"Come on!","Fox starts multishining, because he is the superior character", "Here I come!"};
 	
 	public CharacterA() 
@@ -45,30 +47,62 @@ public class CharacterA implements Dueler{
 	{	
 		int rand0 = (int)(Math.random()*10);
 				
-		if(rand0 < 10)
-		{
-			Curaga();
-		}
 		if(caller instanceof Duel)
 		{
-			int rand = (int)(Math.random()*3);
-			return rand;
+			int rand = (int)(Math.random()*10);
+			
+			if(this.HP >= 30) 
+			{
+				if(rand0 < 7)
+				{
+					return 2;
+				}
+				else if(this.HP < 20) 
+				{
+					return 0;
+					isLoaded = true;
+				}
+				else 
+				{
+					return shoot();
+				}
+			}
+			
+			else 
+			{
+				if(rand0 <= 5) {
+					return 0;
+				}
+				else {
+					shoot();
+			}
 		}
 		
 		return 3;
 	}
 	
-	public void Curaga()
+	/*public void Curaga()
 	{
 		//yes kingdom hearts is amazing
 		currentHP = 100;
-	}
+	*/}
 	
 	public void hit(Object caller) 
 	{
 		if(caller instanceof Duel)
 		{
 			this.currentHP -= 10;
+		}
+	}
+	
+	public int shoot()
+	{
+		if(isLoaded) {
+			return 1;
+			isLoaded = false;
+		}
+		else {
+			return (int)(Math.random()*2);
 		}
 	}
 	
